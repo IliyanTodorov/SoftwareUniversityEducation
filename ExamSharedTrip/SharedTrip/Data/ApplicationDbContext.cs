611b0c7856f68cf37dfc3fa0f6193216f6f6509d
Iliyan Todorov <venomflow2018@gmail.com>
2020-10-13 23:54:39 +0300

    Initial commit

---
+++ ExamSharedTrip/SharedTrip/Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿namespace SharedTrip
+{
+    using Microsoft.EntityFrameworkCore;
+    using SharedTrip.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<UserTrip> UserTrips { get; set; }
+
+        public ApplicationDbContext()
+	    {
+           
+	    }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(DatabaseConfiguration.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTrip>()
+            .HasKey(t => new { t.UserId, t.TripId });
+
+            modelBuilder.Entity<UserTrip>()
+                .HasOne(ut => ut.User)
+                .WithMany(ut => ut.UserTrips)
+                .HasForeignKey(ut => ut.UserId);
+
+            modelBuilder.Entity<UserTrip>()
+                .HasOne(ut => ut.Trip)
+                .WithMany(ut => ut.UserTrips)
+                .HasForeignKey(ut => ut.TripId);                
+        }
+    }
+}
---
+++ ExamSharedTrip/SharedTrip/Models/User.cs
@@ -0,0 +1,30 @@
+﻿namespace SharedTrip.Models
+{
+    using SIS.MvcFramework;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class User
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        public IEnumerable<UserTrip> UserTrips { get; set; }
+
+        [Column(TypeName = "bigint")]
+        public IdentityRole Role { get; set; }
+    }
+}
