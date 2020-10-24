9f242cc4ab9ce42f01a5db071896dd0309376582
Iliyan Todorov <29896848+IliyanTodorov@users.noreply.github.com>
2020-10-24 10:54:28 +0300

    Solve the Exam

--- ExamBattleCards/BattleCards/Data/DatabaseConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BattleCards.Data
-{
-    public class DatabaseConfiguration
-    {
-        public const string ConnectionString =
-            @"Server=ILIYANTODOROV\SQLEXPRESS;Database=BattleCards;Trusted_Connection=True;Integrated Security=True;";
-        //@"Server=ILIYANTODOROV\SQLEXPRESS;Database=BattleCards;Integrated Security=True;";
-    }
-}
